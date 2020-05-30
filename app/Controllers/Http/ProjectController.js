6a4438fc259066c26973b72341497964264bc320
Guilherme Solski Alves <guilhermesolskialves@gmail.com>
2020-05-30 13:21:40 -0300

    Putty route from delete project and task
    Remember that the links in the DB are cascaded, so you need to delete the main table

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -5,13 +5,17 @@ const Project = use("App/Models/Project");
 const Status = use("App/Models/Status");
 const ProjectCompany = use("App/Models/ProjectCompany");
 const CompanyEmployee = use("App/Models/CompanyEmployee");
+const ProjectTask = use("App/Models/ProjectTask");
+const Task = use("App/Models/Task");
+
 
 class ProjectController {
   async get_project(company_id) {
-    const projects = await ProjectCompany.query()
+    const projects = await ProjectCompany
+      .query()
       .where("company_id", company_id)
       .innerJoin("activities", "project_companies.project_id", "activities.id")
-      .select("project_id as id", "title", "description")
+      .select("project_id", "title", "description")
       .fetch();
     return projects;
   }
@@ -19,7 +23,10 @@ class ProjectController {
   async create({ auth, request }) {
     const company_id = auth.user.id;
 
-    const { description, title } = request.only(["description", "title"]);
+    const { description, title } = request.only([
+      "description",
+      "title",
+    ]);
     const trx = await Database.beginTransaction();
 
     // Create Activity
@@ -43,23 +50,21 @@ class ProjectController {
     await ProjectCompany.create(
       {
         company_id,
-        project_id: project.id,
+        project_id: project.id
       },
       trx
     );
 
     //Create based status from project
     await Status.createMany(
-      [
-        {
-          name: "Created",
-          project_id: activity.id,
-        },
-        {
-          name: "Concluded",
-          project_id: activity.id,
-        },
-      ],
+      [{
+        name: "Created",
+        project_id: activity.id
+      },
+      {
+        name: "Concluded",
+        project_id: activity.id
+      }],
       trx
     );
 
@@ -69,50 +74,69 @@ class ProjectController {
 
   async index({ auth }) {
     const user_id = auth.user.id;
+
     try {
-      const company = await CompanyEmployee.findByOrFail(
-        "employee_id",
-        user_id
-      );
-      return await this.get_project(company.id);
+      const company = await CompanyEmployee.findByOrFail("employee_id", user_id);
+      return await this.get_project(company.id)
     } catch (err) {
       return await this.get_project(user_id);
     }
   }
 
   async show({ request, response }) {
-    const { project_id } = request.only(["project_id"]);
+    const { project_id } = request.only([
+      "project_id"
+    ]);
 
-    const project = await Activity.query()
+    const project = await Activity
+      .query()
       .where("activities.id", project_id)
       .innerJoin("statuses", "statuses.project_id", "activities.id")
-      .select(
-        "title",
-        "description",
-        "activities.created_at as created_at",
-        "statuses.name as status_name",
-        "statuses.id as status_id"
-      )
+      .select("title", "description", "activities.created_at as created_at", "statuses.name as status_name", "statuses.id as status_id")
       .fetch();
 
     const projectFormatted = JSON.parse(JSON.stringify(project));
     if (projectFormatted.length === 0) {
-      return response.status(401).send({ error: "Project not found" });
+      return response
+        .status(401)
+        .send({ error: "Project not found" });
     }
     let statuses = [];
     projectFormatted.forEach((values) => {
-      statuses.push({
-        name: values.status_name,
-        id: values.status_id,
-      });
+      statuses.push(
+        {
+          name: values.status_name,
+          id: values.status_id
+        }
+      )
     });
+
 
     return {
       title: projectFormatted[0].title,
       description: projectFormatted[0].description,
       data_entry: projectFormatted[0].data_entry,
-      statuses: statuses,
+      statuses: statuses
     };
+  }
+  async delete({ request, response }) {
+
+    const trx = await Database.beginTransaction();
+
+    const { project_id } = request.only([
+      "project_id"
+    ]);
+
+    try {
+      const project = await Project.findByOrFail("id", project_id);
+      await project.delete(trx);
+      await trx.commit();
+      return true;
+    } catch (err) {
+      return response
+        .status(401)
+        .send({ error: "Project not found" });
+    }
   }
 }
 
--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -14,14 +14,14 @@ class TaskController {
       data_end,
       project_id,
       employees,
-      value,
+      value
     } = request.only([
       "description",
       "title",
       "data_end",
       "project_id",
       "employees",
-      "value",
+      "value"
     ]);
 
     const trx = await Database.beginTransaction();
@@ -40,7 +40,9 @@ class TaskController {
       status = await Status.findByOrFail("project_id", project_id);
     } catch (err) {
       console.log(err);
-      return response.status(401).send({ error: "Project not Found" });
+      return response
+        .status(401)
+        .send({ error: "Project not Found" });
     }
 
     // Create Task
@@ -49,7 +51,7 @@ class TaskController {
         id: activity.id,
         status_id: status.id,
         value,
-        data_end,
+        data_end
       },
       trx
     );
@@ -63,7 +65,7 @@ class TaskController {
       trx
     );
 
-    // Formatted  Employees
+    // Formatted  Employees 
     const employeesFormatted = employees.map((employee) => ({
       employee_id: employee,
       task_id: task.id,
@@ -73,87 +75,98 @@ class TaskController {
       await EmployeeTask.createMany(employeesFormatted, trx);
     } catch (err) {
       console.log(err);
-      return response.status(401).send({ error: "Employee not Found" });
+      return response
+        .status(401)
+        .send({ error: "Employee not Found" });
     }
     await trx.commit();
     return task;
   }
   async show({ request, response }) {
-    const { task_id } = request.only(["task_id"]);
 
-    const task = await Task.query()
+    const { task_id } = request.only([
+      "task_id"
+    ]);
+
+    const task = await Task
+      .query()
       .where("tasks.id", task_id)
       .innerJoin("employee_tasks", "employee_tasks.task_id", "tasks.id")
       .innerJoin("statuses", "statuses.id", "status_id")
       .innerJoin("users", "users.id", "employee_tasks.employee_id")
-      .select(
-        "users.name as employee_name",
-        "users.id as employee_id",
-        "value",
-        "data_end",
-        "tasks.created_at",
-        "statuses.name as status"
-      )
+      .select("users.name as employee_name", "users.id as employee_id", "value",
+        "data_end", "tasks.created_at",
+        "statuses.name as status", "tasks.id as task_id")
       .fetch();
 
     const taskFormatted = JSON.parse(JSON.stringify(task));
     if (taskFormatted.length === 0) {
-      return response.status(401).send({ error: "Task not found" });
+      return response
+        .status(401)
+        .send({ error: "Task not found" });
     }
     let employees = [];
     taskFormatted.forEach((values) => {
-      employees.push({
-        name: values.employee_name,
-        id: values.employee_id,
-      });
+      employees.push(
+        {
+          name: values.employee_name,
+          id: values.employee_id
+        }
+      )
     });
     return {
-      employees: employees,
+      id: taskFormatted[0].task_id,
       value: taskFormatted[0].value,
       data_end: taskFormatted[0].data_end,
       data_entry: taskFormatted[0].created_at,
       status: taskFormatted[0].status,
+      employees: employees,
     };
   }
   async update({ request }) {
-    const { task_id, status_id } = request.only(["task_id", "status_id"]);
+    const { task_id, status_id } = request.only([
+      "task_id", "status_id"
+    ]);
     const trx = await Database.beginTransaction();
-    const task = await Task.findByOrFail("id", task_id);
+    const task = await Task.findByOrFail("id", task_id)
     task.status_id = status_id;
     const save = await task.save(trx);
-    await trx.commit();
+    await trx.commit()
     return save;
   }
 
   async index({ request }) {
-    const { id } = request.params;
-    const tasks = await ProjectTaks.query()
-      .where("project_tasks.project_id", id)
+    const { project_id } = request.only([
+      "project_id"
+    ]);
+    const tasks = await ProjectTaks
+      .query()
+      .where("project_tasks.project_id", project_id)
       .innerJoin("tasks", "project_tasks.task_id", "tasks.id")
       .innerJoin("statuses", "statuses.id", "tasks.status_id")
       .innerJoin("activities", "tasks.id", "activities.id")
-      .select(
-        "task_id as id",
-        "title",
-        "description as content",
-        "value",
-        "statuses.name as status"
-      )
+      .select("title", "description", "value", "statuses.name as status")
       .fetch();
+    return tasks;
+  }
+  async delete({ request,response }) {
 
-    const statuses = await Status.query()
-      .where("project_id", id)
-      .select("id", "name")
-      .fetch();
+    const trx = await Database.beginTransaction();
 
-    const data = statuses.rows.map((status) => ({
-      title: status.$attributes.name,
-      cards: tasks.rows.filter(
-        (task) => status.$attributes.name === task.$attributes.status
-      ),
-    }));
-
-    return data;
+    const { task_id } = request.only([
+      "task_id"
+    ]);
+   
+    try{
+      const task = await Task.findByOrFail("id", task_id);
+      await task.delete(trx);
+      await trx.commit();
+      return true;
+    }catch(err){      
+      return response
+      .status(401)
+      .send({ error: "Task not found" });
+    }
   }
 }
 
